3a42d4135e8b0f3e175511cda1b33be6f4b76b5a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-15 09:54:33 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/test/java/com/fmontalvoo/springboot/webflux/app/SpringbootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
